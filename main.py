a0df46d8115dc593a5179f43ebc95e905b38bab3
Aadi Patwardhan <aadikpat@gmail.com>
2025-05-24 11:44:23 +0200

    Fixed now

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
 import sys, pathlib, qiskit
-<<<<<<< HEAD
-from utils.feature_extractors import extract_all
-from utils.save_utils import save_circuit
-from generator import *
-
-=======
 from feature_extractors.main import extract_all
 from utils.save_utils import save_circuit
 from generator import *
 
-
->>>>>>> 0846e197500375a325dfb8401f6ad8c6fea888b1
 def main():
     print("Hello from inferq!")
 
@@ -26,8 +18,4 @@ if __name__ == "__main__":
     n = int(sys.argv[1]) if len(sys.argv) > 1 else 10
     circ = ghz(n)
     feats = extract_all(circ)
-<<<<<<< HEAD
     save_circuit(circ, feats, pathlib.Path("circuits"))
-=======
-    save_circuit(circ, feats, pathlib.Path("circuits"))
->>>>>>> 0846e197500375a325dfb8401f6ad8c6fea888b1
